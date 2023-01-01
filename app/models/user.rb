17c23fa33a5dd03baf70cc71687197ad4e38a045
Ariel Fogel <fogeltine@gmail.com>
2023-01-01 11:43:24 +0200

    setup user, organization, and membership models

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+
+  validates :name, presence: true
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password,
+    presence: true,
+    length: { minimum: 8 }
+
+  before_validation :strip_whitespace
+
+  private
+
+  def strip_whitespace
+    self.name = self.name&.strip
+    self.email = self.email&.strip
+  end
+end
