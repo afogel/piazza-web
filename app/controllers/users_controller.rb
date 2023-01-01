b2a079327ca9dd2845773c00633da9fe94dd329c
Ariel Fogel <fogeltine@gmail.com>
2023-01-01 14:51:01 +0200

    basic auth system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  skip_authentication only: [:new, :create]
+  
   def new
     @user = User.new
   end
@@ -8,6 +9,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @organization = Organization.create(members: [@user])
+      @app_session = @user.app_sessions.create
+      log_in(@app_session)
+
       redirect_to root_path,
         status: :see_other,
         flash: { success: t(".welcome", name: @user.name) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
+  has_many :app_sessions
 
   validates :name, presence: true
   validates :email,
@@ -14,10 +15,21 @@ class User < ApplicationRecord
 
   before_validation :strip_whitespace
 
+  def authenticate_app_session(app_session_id, token)
+    app_sessions.find(app_session_id).authenticate_token(token)
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+
   private
 
   def strip_whitespace
     self.name = self.name&.strip
     self.email = self.email&.strip
+  end
+
+  def self.create_app_session(email:, password:)
+    return nil unless user = User.find_by(email: email.downcase)
+    user.app_sessions.create if user.authenticate(password) 
   end
 end
